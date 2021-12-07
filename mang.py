dd31bd77d5d89acb46465f370df1aa5bc04042b7
Biohaxzard <79043869+Biohaxzard@users.noreply.github.com>
2021-12-07 11:50:34 +0200

    Update mang.py

--- mang.py
+++ mang.py
@@ -1,17 +1,39 @@
-class Karakter:
-    def __init__(self, nimi, elud=100, tugevus=10):
-        self.nimi = nimi
-        self.elud = elud
-        self.tegevus = tugevus
-    def kaotaelusi(self, elud):
-        self.elud = (elud -10)
-    def lisaelusi(self, elud):
-        self.elud = (elud+10)
-class Vaenlane:
-    def __init__(self, elud=100, tugevus=10):
-        self.elud = (elud - 10)
-        self.tegevus = (tugevus+1)
-    def kaotaelusi1(self, elud):
-        self.elud = elud
-        
-        
+import random
+while True:
+    class Karakter:
+        def __init__(self, nimi, elud, tugevus):
+            self.nimi = nimi
+            self.elud = elud
+            self.tugevus = tugevus
+        def kaotaelusi(self):
+            self.kaotaelusi = random.randint(0,20) 
+            self.elud -= self.kaotaelusi * self.tugevus
+            if self.elud < 0:
+                print("sa kaotasid kõik oma elud ggwp")
+            else:
+                print("kaotasid veic elusi")
+        def lisaelusi(self):
+            if self.elud < 0:
+                print("oled juba dead")
+            else:
+                self.lisaelusi = 20
+                self.lisaelusi = self.elud
+                self.elud += self.lisaelusi
+    class Vaenlane:
+        def __init__(self, elud, tugevus):
+            self.elud = elud
+            self.tugevus = tugevus
+        def kaotaelusi(self):
+            self.kaotaelusi = random.randint(0,20) 
+            self.elud -= self.kaotaelusi * self.tugevus
+            if self.elud < 0:
+                print("vaenlane kaotas kõik oma crypto varanduse ")
+            else:
+                print("vaenlane sai põske veic")
+    break
+karakter = Karakter("peeter", 100, 10)
+vaenlane = Vaenlane(100, 10)
+karakter.kaotaelusi()
+karakter.lisaelusi()
+vaenlane.kaotaelusi()
+print(karakter.elud, vaenlane.elud)
